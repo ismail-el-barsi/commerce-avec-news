92840d998c3f95b8ff6a351f0fa13814e16ce535
ismail-el-barsi <luciferelbarsi@gmail.com>
2025-01-17 11:03:45 +0100

    ajout des screen pour afficher les news

---
+++ frontend/src/screens/NewsScreen.js
@@ -0,0 +1,57 @@
+import axios from 'axios';
+import { useEffect, useReducer } from 'react';
+import { useParams } from 'react-router-dom';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import { getError } from '../utils';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, news: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+function NewsScreen() {
+  const params = useParams();
+  const { slug } = params;
+  const [{ loading, error, news }, dispatch] = useReducer(reducer, {
+    news: [],
+    loading: true,
+    error: '',
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get(
+          `http://localhost:3003/news/slug/${slug}`
+        );
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
+      }
+    };
+    fetchData();
+  }, [slug]);
+
+  return loading ? (
+    <LoadingBox />
+  ) : error ? (
+    <MessageBox variant='danger'>{error}</MessageBox>
+  ) : (
+    <div>
+      <h1>{news.title}</h1> {/* Affiche le titre de l'actualité */}
+      <p>{news.content}</p> {/* Affiche le contenu complet de l'actualité */}
+    </div>
+  );
+}
+
+export default NewsScreen;
